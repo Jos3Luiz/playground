a17c3362538314ad44cfe4243b958263b77ef7eb
Jos3Luiz <zekkarock11@gmail.com>
2020-01-14 20:39:26 -0500

    fix sqlcheat

--- connect_db.php
+++ connect_db.php
@@ -1,11 +1,22 @@
 <?php 
-
-$dbServername="localhost";
+$dbServername="10.30.6.154";
 $dbUsername = "root";
-$dbPassword="";
+$dbPassword="rootpass";
 
-$dbName = "playground";
+
+$dbName = "banco";
 
 $conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
+// Check connection
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Print host information
+echo "Connect Successfully. Host info: " . mysqli_get_host_info($link);
+ 
+
+
+
 
 ?>
